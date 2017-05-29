53fa8f6234761ec1b21d70b8079d64e3cfb23ca3
wallenius71 <jessetrichards@gmail.com>
2017-05-29 11:50:32 -0700

    addig files to new repository

---
+++ WindART.Data/WindART_DAL/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration ;
+
+
+namespace WindART.DAL
+{  
+    public enum DataSourceType
+        {
+            //type for a datasource 
+            XL2003,
+            XL2007,
+            SQL2005,
+            OSIPI,
+            CSV,
+            TXT,
+            PRN,
+            DAT,
+            WindART_PI,
+            WindART_SQL,
+            Operations_PI,
+            Cedar_Creek,
+            Edom_Hills,
+            Flat_Ridge,
+            Flat_Ridge_1,
+            Fowler_01,
+            Fowler_02,
+            Goshen,
+            Silver_Star,
+            Titan,
+            Trinity,
+            Sherbino_Mesa_2,
+            Sherbino_Mesa_1,
+            Mehoopany,
+            NotSet
+        }
+
+
+}
